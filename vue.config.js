c1085c69c6f339eda267c79551ab448c3d6da503
Antonio José Masiá <antonio.masia@frogtek.org>
2021-02-08 16:47:22 +0100

    Update CI/CD and test version

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      builderOptions: {
-        publish: [
-          {
-            provider: 'github',
-            owner: 'ajmasia',
-            repo: 'vue-electron-playground',
-          },
-        ],
+      electronBuilder: {
+        builderOptions: {
+          publish: ['github'],
+        },
       },
     },
   },
